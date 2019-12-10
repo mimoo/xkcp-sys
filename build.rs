96454d5bf12cdc55e7f30e152f98516ea8b36255
David Wong <davidwg@calibra.com>
2019-12-10 15:23:12 -0800

    build script should be working now

--- build.rs
+++ build.rs
@@ -1,5 +1,17 @@
 fn main() {
-    //    let XKCP_dir = env!("CARGO_MANIFEST_DIR").join("XKCP");
+    let xkcp_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("XKCP");
+
+    std::process::Command::new("git")
+        .args(&["submodule", "init"])
+        .status()
+        .expect("failed to init git submodule XKCP");
+
+    std::process::Command::new("git")
+        .args(&["submodule", "update"])
+        .status()
+        .expect("failed to update git submodule XKCP");
+
+    assert!(std::env::set_current_dir(&xkcp_dir).is_ok());
 
     std::process::Command::new("make")
         .args(&["generic64/libkeccak.a"])
