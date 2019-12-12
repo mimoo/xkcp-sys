65590daa045cf76eb0bca1877af6037fb506dcbf
David Wong <davidwg@calibra.com>
2019-12-12 11:25:53 -0800

    build knows when it has to re-build due to xkcp version change

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
 fn main() {
-    // current dir
-    let main_dir = std::env::current_dir().unwrap();
     // the OUT_DIR where we clone and build XKCP
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     // shortcut to the XKCP dir in OUT_DIR
@@ -11,11 +9,14 @@ fn main() {
     // ==============
     //
 
+    // obtain version of XKCP from .xkcp_version
+    let xkcp_version = std::fs::read_to_string(".xkcp_version").unwrap();
+
     // cd OUT_DIR
     assert!(std::env::set_current_dir(&out_dir).is_ok());
 
     // remove any XKCP that is already lying there
-    std::fs::remove_dir_all(&xkcp_dir);
+    std::fs::remove_dir_all(&xkcp_dir).ok();
 
     // git clone https://github.com/XKCP/XKCP.git
     // git checkout e8e6b1d45861ea6fc0957ab27a401f42aef0d033
@@ -25,7 +26,7 @@ fn main() {
         .expect("failed to git clone XKCP");
 
     std::process::Command::new("git")
-        .args(&["checkout", "e8e6b1d45861ea6fc0957ab27a401f42aef0d033"])
+        .args(&["checkout", &xkcp_version])
         .status()
         .expect("failed to git checkout a specific commit");
 
@@ -42,8 +43,8 @@ fn main() {
     // Cargo flags
     //
 
-    // tell cargo to re-build if our wrapper.h changes
-    println!("cargo:rerun-if-changed=wrapper.h");
+    // tell cargo to re-build if our .xkcp_version changes
+    println!("cargo:rerun-if-changed=.xkcp_version");
     // tell cargo to use the library path OUT_DIR/XKCP/bin/generic64
     println!("cargo:rustc-flags=-L {}/XKCP/bin/generic64", out_dir.display());
 }
