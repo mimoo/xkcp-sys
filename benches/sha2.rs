819403a8160aea1d55cc7f76df754e7862df0319
David Wong <davidwg@calibra.com>
2019-12-12 17:55:09 -0800

    adding a bench for the crate sha2 (different from ring sha2)

--- benches/sha2.rs
+++ benches/sha2.rs
@@ -1,15 +1,11 @@
 #[macro_use]
 extern crate criterion;
 
-use ring::digest;
-
-fn criterion_benchmark(c: &mut criterion::Criterion) {
-
-
-    let mut ctx = digest::Context::new(&digest::SHA256);
+fn bench_ring_sha2(c: &mut criterion::Criterion) {
+    let mut ctx = ring::digest::Context::new(&ring::digest::SHA256);
     ctx.update(b"testing");
 
-    c.bench_function("hashing", |b| b.iter(|| {
+    c.bench_function("hashing with ring", |b| b.iter(|| {
         let mut ctx = ctx.clone();
         ctx.update(criterion::black_box(b"someinput"));
         let digest = ctx.finish();
@@ -17,5 +13,19 @@ fn criterion_benchmark(c: &mut criterion::Criterion) {
     }));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_crates_sha2(c: &mut criterion::Criterion) {
+    use sha2::Digest;
+
+    let mut hasher = sha2::Sha256::new();
+    hasher.input(b"testing");
+
+    c.bench_function("hashing with sha2", |b| b.iter(|| {
+        let mut hasher = hasher.clone();
+        hasher.input(criterion::black_box(b"someinput"));
+        let digest = hasher.result();
+        assert_eq!(digest.as_ref(), [126, 95, 252, 85, 113, 109, 112, 3, 11, 130, 133, 70, 74, 104, 99, 129, 176, 158, 55, 233, 104, 71, 174, 0, 214, 183, 146, 102, 114, 46, 115, 246]);
+    }));
+}
+
+criterion_group!(benches, bench_ring_sha2, bench_crates_sha2);
 criterion_main!(benches);
